9b75c83f5d281d324943159cbe0ba3c5c8b6fc31
korpi <os2777ko-s@student.lu.se>
2024-07-06 00:03:52 +0200

    build: dockerize

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,22 @@
+import io.ktor.plugin.features.*
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
 val exposed_version: String by project
 val h2_version: String by project
+val projectVersion: String = "0.0.1"
 
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "2.0.0"
     id("io.ktor.plugin") version "2.3.1"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.22"
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0"
 }
 
 group = "eu.filebridge"
-version = "0.0.1"
+version = projectVersion
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
-    val isDevelopment: Boolean = project.ext.has("development")
-    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 repositories {
@@ -32,8 +32,6 @@ dependencies {
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-request-validation:$ktor_version")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
@@ -46,4 +44,12 @@ dependencies {
     implementation("io.ktor:ktor-server-cors-jvm:2.3.1")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+ktor {
+    docker {
+        jreVersion.set(JreVersion.JRE_17)  // Documentation says that we should use JavaVersion. but gradle is a piece of shit, I guess this is how it's done
+        localImageName.set("filebridge-backend")
+        imageTag.set(projectVersion)
+    }
 }
