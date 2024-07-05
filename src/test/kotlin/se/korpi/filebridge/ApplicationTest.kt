038daad6ab0d6f3ee2d0674966e2afe4245b457a
korpi <os2777ko-s@student.lu.se>
2024-07-06 00:22:55 +0200

    fix: import error

--- src/test/kotlin/se/korpi/filebridge/ApplicationTest.kt
+++ src/test/kotlin/se/korpi/filebridge/ApplicationTest.kt
@@ -5,7 +5,6 @@ import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
-import eu.filebridge.plugins.*
 import se.korpi.filebridge.plugins.configureRouting
 
 class ApplicationTest {
